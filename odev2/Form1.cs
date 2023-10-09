dcb42c879c786c31262b1f4e576aa9b00d311db2
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-10-09 11:36:11 +0300

    Add files via upload

---
+++ odev2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string kulanici_adi;
+            kulanici_adi = textBox1.Text;
+            if(kulanici_adi == "miliegitim@meb.k12.tr")
+            {
+            MessageBox.Show("kullanıcı sisteme kayıtlıdır.");
+            }
+            else
+            {
+            MessageBox.Show("kullanıcı adınız yanlıştır.");
+            }
+        }
+    }
+}
